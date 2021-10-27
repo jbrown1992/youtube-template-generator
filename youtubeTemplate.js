689a60ab9a2388c0d10c68c66ec9aa55ae30365d
James Brown <jamesbrown19921992@hotmail.com>
2021-10-27 17:19:47 +0100

    youtube

--- youtubeTemplate.js
+++ youtubeTemplate.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer')
 const clipboardy = require('clipboardy');
 
-
 inquirer
   .prompt([
     {
@@ -17,7 +16,31 @@ inquirer
   ])
   .then((answer) => {
     //console.log("Hello", answer.company, answer.ticker);
-    var result=`${answer.ticker}, ${answer.company},${answer.ticker} stock, ${answer.company} stock, ${answer.ticker} stock analysis, ${answer.company} stock analysis, ${answer.ticker} stock 2021, ${answer.company} stock 2021, buy ${answer.ticker}, buy ${answer.company}, buy ${answer.ticker} stock, buy ${answer.company} stock, ${answer.ticker} overvalued, ${answer.ticker} undervalued, ${answer.company} overvalued, ${answer.company} undervalued, stock analysis, stock market analysis, stock market, investing, financial statement analysis, balance sheet, income statement, stocks to buy now, fundamental analysis, should i buy ${answer.company} stock, should i buy ${answer.ticker} stock`
+    var result=`${answer.ticker},
+    ${answer.company},
+    ${answer.ticker} stock, 
+    ${answer.company} stock, 
+    ${answer.ticker} stock analysis, 
+    ${answer.company} stock analysis, 
+    ${answer.ticker} stock review, 
+    ${answer.company} stock review,
+    ${answer.ticker} stock 2021, 
+    ${answer.company} stock 2021, 
+    ${answer.ticker} stock price prediction, 
+    ${answer.company} stock price prediction,
+    ${answer.ticker} stock forecast, 
+    ${answer.company} stock forecast,
+    buy ${answer.ticker}, 
+    buy ${answer.company}, 
+    buy ${answer.ticker} stock, 
+    buy ${answer.company} stock, 
+    ${answer.ticker} undervalued, 
+    ${answer.company} undervalued, 
+    should i buy ${answer.company} stock, 
+    should i buy ${answer.ticker} stock,
+    is ${answer.company} a good investment,
+    is ${answer.ticker} a good investment,
+    ${answer.company} fundamental analysis,`
     console.log(result)
     clipboardy.writeSync(result);
 });
